bcc649d893fc5b91c23b2ca17dd795da4ab170d7
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-10-21 12:06:46 +0300

    Structure for web site was created: all web pages and basic html. Some basic style was added

--- index.php
+++ index.php
@@ -1,12 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tervetuloa Kurssienhallintaan</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1>Tervetuloa Kurssienhallintaan</h1>
-    
+    <h2>Valitse toiminto</h2>
+    <div class="lists-wrapper">
+        <ul class="action-list">
+            <li class="action-list-item">
+                <a href="./add-student.php" class="action-list-link">Lisää opiskelija</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./edit-delete-student.php" class="action-list-link">Poista/muokkaa opiskelija</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./add-teacher.php" class="action-list-link">Lisää opettaja</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./edit-delete-teacher.php" class="action-list-link">Poista/muokkaa opettaja</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./add-course.php" class="action-list-link">Lisää kurssi</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./edit-delete-course.php" class="action-list-link">Poista/muokkaa kurssi</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./add-auditory.php" class="action-list-link">Lisää tila</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./edit-delete-auditory.php" class="action-list-link">Poista/muokkaa tila</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./course-registration.php" class="action-list-link">Lisää kurssikirjautuminen</a>
+            </li>
+            <li class="action-list-item">
+                <a href="./edit-delete-registration.php" class="action-list-link">Poista/muokkaa kurssikirjautuminen</a>
+            </li>
+        </ul>
+        <ul class="review-list">
+            <li class="review-list-item">
+                <a href="./get-student-info.php" class="review-list-link">Nähdä opiskelijan tiedot</a>
+            </li>
+            <li class="review-list-item">
+                <a href="./get-teacher-info.php" class="review-list-link">Nähdä opettajan tiedot</a>
+            </li>
+            <li class="review-list-item">
+                <a href="./get-auditory-info.php" class="review-list-link">Nähdä tilan tiedot</a>
+            </li>
+            <li class="review-list-item">
+                <a href="./get-course-info.php" class="review-list-link">Nähdä kurssin tiedot</a>
+            </li>
+            <li class="review-list-item">
+                <a href="./get-course-registration-info.php" class="review-list-link">Nähdä kurssikirjautumisen tiedot</a>
+            </li>
+        </ul>
+    </div>
+
+
 </body>
+
 </html>
