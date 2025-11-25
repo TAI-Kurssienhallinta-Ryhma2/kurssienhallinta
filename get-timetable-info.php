7a8f9ef741a67f7e9f9704f583de185ff0f0911c
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-11-25 12:43:43 +0200

    Select element with number of week and date interval has done

--- get-timetable-info.php
+++ get-timetable-info.php
@@ -18,8 +18,20 @@ if (isset($_GET["auditory-id"]) && $_GET["auditory-id"] !== null) {
     $_SESSION["course_id"] = $_GET['course-id'];
 }
 
+//Get today data:
+date_default_timezone_set("Europe/Helsinki");
+$today_date = date("Y.m.d");
+$current_week = date("W");
+$current_year = date("Y");
+$d = new DateTime();
+$d->setISODate($current_year, $current_week);
+$current_start_of_week = $d->format("d.m.y");
+$d->modify('+6 days');
+$current_end_of_week = $d->format("d.m.y");
+
 echo "<pre>";
-// print_r($registration_portion);
+//  print_r($current_week);
+//  print_r($current_year);
 echo "</pre>";
 ?>
 
@@ -34,9 +46,10 @@ echo "</pre>";
 </head>
 
 <body>
-<a href="./index.php" class="go-back-btn">Palaa pääsivulle</a>
-<h1>Tarkastele aikataulua</h1>
-<div class="filters-wrapper">
+    <a href="./index.php" class="go-back-btn">Palaa pääsivulle</a>
+    <h1>Tarkastele aikataulua</h1>
+
+    <div class="filters-wrapper">
         <h2>Suodattimet</h2>
         <div class="filters">
             <!-- <label for="courses">Valitse kurssi:</label> -->
@@ -133,10 +146,55 @@ echo "</pre>";
         </div>
     </div>
 
-    <script>
+    <div class="week-filter-wrapper">
+        <div class="filters">
+            <label for="choose-week-btn">Valitse jakso:</label>
+            <!-- Form the dropdown menu for week selection: -->
+            <select name="choose-week-btn" id="choose-week-btn" class="choose-week-btn">
+                <!-- Form 4 records before "current" week: -->
+                <?php
+                $week_before = $current_week - 5;
+                for ($i = 0; $i < 4; $i++) {
+                    $week_before = $week_before + 1;
+                    $db = new DateTime();
+                    $db->setISODate($current_year, $week_before);
+                    $start_of_week = $db->format("d.m.y");
+                    $db->modify('+6 days');
+                    $end_of_week = $db->format("d.m.y");
+                ?>
+                    <option id="week-<?php echo $week_before; ?>-year-<?php echo $current_year; ?>" value="<?php echo $week_before . "/" . $current_year; ?>"><?php echo $week_before . "/" . $current_year . " (" . $start_of_week . " - " . $end_of_week . ")"; ?></option>
+                <?php
+                }
+                ?>
+                <!-- "Current" week: -->
+                <option selected id="week-<?php echo $current_week; ?>-year-<?php echo $current_year; ?>" value="<?php echo $current_week . "/" . $current_year; ?>"><?php echo $current_week . "/" . $current_year. " (" . $current_start_of_week . " - " . $current_end_of_week . ")"; ?></option>
 
+                <!-- Form 4 records after "current" week: -->
+                <?php
+                $week_after = $current_week;
+                for ($i = 0; $i < 4; $i++) {
+                    $week_after = $week_after + 1;
+                    $da = new DateTime();
+                    $da->setISODate($current_year, $week_after);
+                    $start_of_week = $da->format("d.m.y");
+                    $da->modify('+6 days');
+                    $end_of_week = $da->format("d.m.y");
+                ?>
+                    <option id="week-<?php echo $week_after; ?>-year-<?php echo $current_year; ?>" value="<?php echo $week_after . "/" . $current_year; ?>"><?php echo $week_after . "/" . $current_year . " (" . $start_of_week . " - " . $end_of_week . ")"; ?></option>
+                <?php
+                }
+                ?>
+
+            </select>
+        </div>
+
+    </div>
+
+
+
+    <script>
         // script to observe the option selection event for courses:
-            const selectElements = document.querySelectorAll("select");
+        const selectElements = document.querySelectorAll("select");
         selectElements.forEach(selectElement => {
             selectElement.addEventListener('change', formAddressPath);
         });
@@ -162,8 +220,6 @@ echo "</pre>";
                     break;
             }
         }
-
-
     </script>
 
 </body>
